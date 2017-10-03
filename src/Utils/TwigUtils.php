f4b877828ba36060211522390464a7e07f8ce671
Gary Tierney <gary.tierney@digirati.com>
2017-10-03 13:57:08 +0100

    Add TwigUtils docblocks

--- src/Utils/TwigUtils.php
+++ src/Utils/TwigUtils.php
@@ -4,10 +4,19 @@
 namespace Digirati\ExtractTwigStrings\Utils;
 
 
+/**
+ * Static utilities related to the parsing of Twig files.
+ */
 class TwigUtils
 {
     private function __construct() {}
 
+    /**
+     * Return a factory to shim any symbols that can't be resolved in the Twig template being parsed.
+     *
+     * @param string $type
+     * @return \Closure
+     */
     public static function undefinedSymbolHandler(string $type) {
         return function ($name) use ($type) {
             return new $type($name);
